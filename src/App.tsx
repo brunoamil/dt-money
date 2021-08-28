f5b206a2186da15a916c97bda441cab90c86e9ff
Bruno <bruno-amil@hotmail.com>
2021-08-28 00:14:51 -0300

    formatando datas

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,36 @@ createServer({
   models: {
     transaction: Model,
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer de Website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Cabeca de Website",
+          type: "withdraw",
+          category: "DSX",
+          amount: 6000,
+          createdAt: new Date("2021-02-22 09:00:00"),
+        },
+        {
+          id: 3,
+          title: "Teste de Website",
+          type: "withdraw",
+          category: "XX",
+          amount: 6000,
+          createdAt: new Date("2021-02-14 09:00:00"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
 
